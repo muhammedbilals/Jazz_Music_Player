f269b6c56673ec8c14c7539ac179ba122952b777
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-04-11 12:10:05 +0530

    update in mostplayed

--- lib/logic/bloc/mostplayed_bloc.dart
+++ lib/logic/bloc/mostplayed_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 import 'package:music_player/model/mostplayed.dart';
 import 'package:equatable/equatable.dart';
+import 'package:music_player/model/recentlyplayed.dart';
 
 part 'mostplayed_event.dart';
 part 'mostplayed_state.dart';
@@ -13,6 +14,24 @@ class MostplayedBloc extends Bloc<MostplayedEvent, MostplayedState> {
       final List<MostPlayed> mostplay = mostbox.values.toList();
 
       emit(DisplayRecentlyPlayed(mostplay));
+    });
+    on<UpdateMostPlayedCount>((event, emit) {
+        final box = MostplayedBox.getInstance();
+  List<MostPlayed> list1 = box.values.toList();
+  bool isAlready =
+      list1.where((element) => element.songname == event.mostplay.songname).isEmpty;
+  if (isAlready == true) {
+    box.add(event.mostplay);
+  } else {
+    
+    int index =
+        list1.indexWhere((element) => element.songname == event.mostplay.songname);
+    box.deleteAt(index);
+    // mostplayedsongs.add(event.mostplay);
+    box.put(index, event.mostplay);
+  }
+  int count = event.mostplay.count;
+  event.mostplay.count = count + 1;
     });
   }
 }
--- lib/logic/bloc/mostplayed_event.dart
+++ lib/logic/bloc/mostplayed_event.dart
@@ -6,3 +6,13 @@ abstract class MostplayedEvent {}
 class FetchRecentlyPlayed extends MostplayedEvent {
   FetchRecentlyPlayed();
 }
+
+class UpdateMostPlayedCount extends MostplayedEvent {
+  final MostPlayed mostplay;
+  final int index;
+
+  UpdateMostPlayedCount(this.mostplay, this.index);
+  
+      @override
+  List<Object> get props => [mostplay,index];
+}
